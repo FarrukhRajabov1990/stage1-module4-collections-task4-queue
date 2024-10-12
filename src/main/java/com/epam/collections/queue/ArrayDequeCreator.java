9a01ad09302a6cf62f9a6befe9c8f07c8bcbcd0e
Farrukh_Rajabov <Farruhrajabov2012@gmail.com>
2024-10-12 11:10:22 +0500

    Initial commit

--- src/main/java/com/epam/collections/queue/ArrayDequeCreator.java
+++ src/main/java/com/epam/collections/queue/ArrayDequeCreator.java
@@ -6,5 +6,22 @@ import java.util.Queue;
 
 public class ArrayDequeCreator extends PriorityQueue<String> {
     public ArrayDeque<Integer> createArrayDeque(Queue<Integer> firstQueue, Queue<Integer> secondQueue) {
+
+        ArrayDeque<Integer> result = new ArrayDeque<>();
+        result.add(firstQueue.remove());
+        result.add(firstQueue.remove());
+        result.add(secondQueue.remove());
+        result.add(secondQueue.remove());
+
+        while (!firstQueue.isEmpty() || !secondQueue.isEmpty()) {
+            firstQueue.add(result.remove());
+            result.add(firstQueue.remove());
+            result.add(firstQueue.remove());
+            secondQueue.add(result.remove());
+            result.add(secondQueue.remove());
+            result.add(secondQueue.remove());
+        }
+
+        return result;
     }
 }
